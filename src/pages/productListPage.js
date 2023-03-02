9355feadae561618a28afa2bb3cb13dca8d14d78
Maciej Wąsowski <maciejw1995n@gmail.com>
2023-03-02 16:15:45 +0100

    products change

--- src/pages/productListPage.js
+++ src/pages/productListPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/ProductListPage.css';
 
-const products = ["Tokyo", "London", "New York"]
+const products = ["London", "Tokyo", "Chicago"]
 const ProductListPage = () => {
 
     const list = products.map(product => (
